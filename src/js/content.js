dd588911f54e794e727977e8ee38098380ceaaba
priyankadhulkhed <priyanka.dhulkhed@gmail.com>
2020-12-07 23:30:13 -0500

    content_script

---
+++ src/js/content.js
@@ -0,0 +1,20 @@
+// alert("webnotate");
+console.log('hiii');
+// $( document ).ready(function() {
+//     var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+//     console.log("Hello from annot " + firstHref);    // Your code here.
+
+//     var t = '';
+//     function gText(e) {
+//         t = document.getSelection ? document.getSelection().toString() :  document.selection.createRange().toString();
+
+//         console.log(t.toString())
+
+//         // create a popper here
+//     }
+
+//     document.onmouseup = gText;
+//     if (!document.all) document.captureEvents(Event.MOUSEUP);
+
+// });
