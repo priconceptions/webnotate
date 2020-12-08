319967d69d9b9a6991208c6861a93f7c8a3dfd4b
priyankadhulkhed <priyanka.dhulkhed@gmail.com>
2020-12-07 22:43:47 -0500

    popup heading

--- src/js/popup/Popup.js
+++ src/js/popup/Popup.js
@@ -1,31 +1,27 @@
-import React from "react"
-import { ChakraProvider } from "@chakra-ui/react";
-import { Box, Heading } from "@chakra-ui/react";
-import { hot } from "react-hot-loader";
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
+import { hot } from 'react-hot-loader';
 
 const Popup = () => {
-    return (
+  return (
     <ChakraProvider>
-        <Box w="350px" h="550px"
-                overflow="hidden"
-                p={2}
+      <Box w="350px" h="550px" overflow="hidden" p={2}>
+        <Heading
+          as="h1"
+          size="xl"
+          textAlign="center"
+          m={0}
+          py={15}
+          borderBottomStyle="solid"
+          borderBottomWidth="1px"
+          borderColor="gray.400"
         >
-            <Heading as="h1"
-                size="xl"
-                textAlign="center"
-                m={0}
-                py={15}
-                borderBottomStyle="solid"
-                borderBottomWidth="1px"
-                borderColor="gray.400"
-            >
-                    Webnotate
-            </Heading>
-            
-        
-            </Box>
-</ChakraProvider>
-    );
-}
+          Webnotate
+        </Heading>
+      </Box>
+    </ChakraProvider>
+  );
+};
 
 export default hot(module)(Popup);
